3cc6e777849fd7414577ef2935d693598b45ce0a
nichokevin <nicholaskevin28@gmail.com>
2020-04-16 18:43:41 +0700

    2 files modified- Array, For, If

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Kevin';
+
+  //array
+  item = [{
+    nama : 'Buah jeruk',
+    harga : 10000
+  },{
+    nama : 'Buah naga',
+    harga : 20000
+  }];
+  itemArr = [1,2,3,4,5];
+
+  //if
+  show: boolean = true;
+
+  user={
+    nama: 'abc'
+  }
+  
 }
